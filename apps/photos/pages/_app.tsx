8f6dda9de89f3e86c1771f8c96a5f692725a5dc4
std4453 <me@std4453.com>
2023-01-08 02:59:23 +0800

    feat(photos): 添加icon

--- apps/photos/pages/_app.tsx
+++ apps/photos/pages/_app.tsx
@@ -17,7 +17,10 @@ function CustomApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover"
         />
-        <title>摄影</title>
+        <title>摄影 - std4453</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <main draggable="false">
         <Component {...pageProps} />
