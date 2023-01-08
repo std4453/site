9ab5d912a759521bd8733fa9e5cb3de9464f7774
std4453 <me@std4453.com>
2023-01-09 04:22:55 +0800

    feat(photos): 错误页意义不明小动画

--- apps/photos/components/error-page/index.tsx
+++ apps/photos/components/error-page/index.tsx
@@ -1,8 +1,9 @@
 import { css, Global } from '@emotion/react';
+import { useMemoizedFn } from 'ahooks';
 import Logo from 'components/logo';
 import Image from 'next/image';
 import moon from 'public/images/image_2924-Moon-Dust-Cloud.png';
-import { ReactNode } from 'react';
+import { MouseEvent, ReactNode, TouchEvent, useRef } from 'react';
 import { fontFamily } from 'utils/fonts';
 
 export interface ErrorPageProps {
@@ -23,6 +24,28 @@ export function ErrorPage({
   actionText = null,
   onActionButtonClick,
 }: ErrorPageProps) {
+  const moonRef = useRef<HTMLDivElement>();
+  const imageRef = useRef<HTMLImageElement>();
+  const handleMoonMouseDown = useMemoizedFn((e: MouseEvent | TouchEvent) => {
+    if (!moonRef.current || !imageRef.current) {
+      return;
+    }
+    const clientX = 'touches' in e ? e.touches[0].clientX : e.clientX;
+    const clientY = 'touches' in e ? e.touches[0].clientY : e.clientY;
+    const rect = moonRef.current.getBoundingClientRect();
+    const cx = (clientX - rect.x) / rect.width;
+    const cy = (clientY - rect.y) / rect.height;
+    imageRef.current.style.transformOrigin = `${cx * 100}% ${cy * 100}%`;
+    imageRef.current.style.transform = 'scale(0.8)';
+  });
+  const handleMoonMouseUp = useMemoizedFn(() => {
+    if (!moonRef.current || !imageRef.current) {
+      return;
+    }
+    imageRef.current.style.transformOrigin = `50% 50%`;
+    imageRef.current.style.transform = 'scale(1)';
+  });
+
   return (
     <>
       <Global
@@ -51,6 +74,9 @@ export function ErrorPage({
             width: 23rem;
             height: 12rem;
             position: relative;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
           `}
         >
           <div
@@ -60,6 +86,9 @@ export function ErrorPage({
               right: 0;
               width: 100%;
               height: 100%;
+
+              pointer-events: none;
+              user-select: none;
 
               & svg {
                 width: 100%;
@@ -74,13 +103,19 @@ export function ErrorPage({
           </div>
           <div
             css={css`
-              position: absolute;
-              top: 0;
               height: 100%;
-              left: 50%;
-              transform: translateX(-50%);
               aspect-ratio: 1;
+              position: relative;
+
+              user-select: none;
+              -webkit-tap-highlight-color: transparent;
             `}
+            ref={moonRef}
+            onTouchStart={handleMoonMouseDown}
+            onMouseDown={handleMoonMouseDown}
+            onMouseUp={handleMoonMouseUp}
+            onTouchEnd={handleMoonMouseUp}
+            draggable="false"
           >
             <Image
               css={css`
@@ -88,7 +123,11 @@ export function ErrorPage({
                 height: 100%;
                 display: block;
                 object-fit: contain;
+                pointer-events: none;
+                transform-origin: center center;
+                transition: transform 100ms ease, transform-origin 70ms linear;
               `}
+              ref={imageRef}
               src={moon}
               alt={'"Moon" by Gregory H. Revera / CC BY-SA 3.0'}
             />
@@ -100,6 +139,9 @@ export function ErrorPage({
               right: 0;
               width: 100%;
               height: 100%;
+
+              pointer-events: none;
+              user-select: none;
 
               & svg {
                 width: 100%;
