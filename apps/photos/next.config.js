a90e00bad91ef1fc7ada864c056c4d48d19c6795
std4453 <me@std4453.com>
2023-01-15 16:26:05 +0800

    fix(photos): dev环境不使用bundle analyzer

--- apps/photos/next.config.js
+++ apps/photos/next.config.js
@@ -26,10 +26,11 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 });
 
-// There's currently no native way to specify environment variables from nx 
+// There's currently no native way to specify environment variables from nx
 // project.json, one should add ANALYZE to .env.local
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled:
+    process.env.ANALYZE === 'true' && process.env.NODE_ENV !== 'development',
 });
 
 module.exports = withSentryConfig(
